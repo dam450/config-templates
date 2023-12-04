19d0b8f777d6d34aa01931bab516d91010671654
Evandro Damaso <68165752+dam450@users.noreply.github.com>
2023-12-04 13:56:47 -0300

    chore: config path alias

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+
+  resolve: {
+    alias: {
+      // eslint-disable-next-line no-undef
+      '@': resolve(__dirname, 'src')
+    }
+  }
 })
